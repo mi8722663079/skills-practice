593e306fb008638600c69b33fc015d1d2663acfd
mi8722663079 <mi8722663@gmail.com>
2023-09-12 13:40:37 +0300

    a bit of a progress

---
+++ script.js
@@ -0,0 +1,7 @@
+const root = document.querySelector(":root");
+const content = document.querySelector(".content");
+const panels = document.querySelectorAll(".element");
+const panel1 = document.querySelector(".e1");
+const panel2 = document.querySelector(".e2");
+const panel3 = document.querySelector(".e3");
+const rs = getComputedStyle(root);
