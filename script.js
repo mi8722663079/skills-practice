6bfeb506966ae138ff61f9a84e9889c1a8db6ca4
mi8722663079 <mi8722663@gmail.com>
2023-09-12 16:51:59 +0200

    did the slide animation, but it still needs work

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const panel1 = document.querySelector(".e1");
 const panel2 = document.querySelector(".e2");
 const panel3 = document.querySelector(".e3");
 const rs = getComputedStyle(root);
+
+function slide1() {
+  panel1.style["left"] = "29%";
+  panel1.style["animation"] = "0.3s ease-in slide1";
+}
