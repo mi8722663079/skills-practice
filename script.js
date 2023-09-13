2271511e9266c75355d9f605e93a4381d944c428
mi8722663079 <mi8722663@gmail.com>
2023-09-13 19:17:48 +0200

    animation progress

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ const panel1 = document.querySelector(".e1");
 const panel2 = document.querySelector(".e2");
 const panel3 = document.querySelector(".e3");
 const rs = getComputedStyle(root);
+const p1 = getComputedStyle(panel1);
 
-function slide1() {
-  panel1.style["left"] = "29%";
-  panel1.style["animation"] = "0.3s ease-in slide1";
-}
+const transfromation = [{ transfrom: "scale(1)" }, { transfrom: "scale(1.5)" }];
+
+panel1.addEventListener("click", () => {
+  panel1.animate(
+    [
+      {
+        transform: "translateX(0px)",
+      },
+      {
+        transform: "translateX(222px)",
+      },
+    ],
+    {
+      duration: 800,
+    }
+  );
+  panel1.classList.add(".pos");
+});
+
+console.log(panel1);
